cfc5f1b26b24b7389835bbbb6018914e7677196e
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-05-09 12:22:30 +1200

    Likelihood fix

--- Code/StarFieldModel.cpp
+++ Code/StarFieldModel.cpp
@@ -296,7 +296,7 @@ void StarFieldModel::calculateLogLikelihood()
 					+ noiseCoeff*(mock(i, j) - background);
 
 				logL += -0.5*log(2*M_PI*var)
-					- 0.5*pow(Data::get_instance().get_image(0)(i, j)
+					- 0.5*pow(Data::get_instance().get_image(k)(i, j)
 					- mockImage(i, j), 2)/var;
 			}
 	}
